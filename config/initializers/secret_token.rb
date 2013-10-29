9025e8fcb4c582dc9edac52aeb85ce2b4e79a27f
Shawn Catanzarite <me@shawncatz.com>
2013-10-29 13:03:38 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hanza::Application.config.secret_key_base = '34cb9fffcafa0e512451b90f79087e90bb9487dc70f7e3d55d5828fc2a98460804685d044a9714b5f769ac8f999b2cae78d3f54b78ac172f3137e071b55dcf07'
